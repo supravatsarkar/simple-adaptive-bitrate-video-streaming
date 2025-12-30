654fb998ba3c1327116ba8e982e1c80870da8fa6
Supravat <supravatsarkar18@gmail.com>
2025-12-30 17:23:21 +0530

    Added video list api

--- video-streaming-backend/src/controllers/videoUplaod.controller.ts
+++ video-streaming-backend/src/controllers/videoUplaod.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { processVideoForHls } from "../services/video.service";
 import fs from "fs";
+import { readdir } from "fs/promises";
 export const videoUploadController = (req: Request, res: Response) => {
   if (!req.file) {
     return res.status(400).json({
@@ -28,4 +29,24 @@ export const videoUploadController = (req: Request, res: Response) => {
       data: `/${masterPlaylistPath}`,
     });
   });
+};
+
+export const getVideoListController = async (req: Request, res: Response) => {
+  try {
+    const directoryPath: string = process.cwd() + "/videos_output";
+    console.log(directoryPath);
+    const files = await readdir(directoryPath);
+    const filteredFiles = files.filter((file) => !file.startsWith("."));
+    console.log(filteredFiles);
+    return res.status(200).json({
+      success: true,
+      data: filteredFiles,
+    });
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong!",
+    });
+  }
 };
--- video-streaming-backend/src/routes/v1/video.routes.ts
+++ video-streaming-backend/src/routes/v1/video.routes.ts
@@ -1,7 +1,11 @@
 import express from "express";
-import { videoUploadController } from "../../controllers/videoUplaod.controller";
+import {
+  videoUploadController,
+  getVideoListController,
+} from "../../controllers/videoUplaod.controller";
 import { upload } from "../../middlewares/multer.middleware";
 
 const videoRouter = express.Router();
 videoRouter.post("/upload", upload.single("video"), videoUploadController);
+videoRouter.get("/list", getVideoListController);
 export default videoRouter;
